32f3727cd7e587c6288a4640a2ea3674680bb495
Stephen Neely <Stephen.Neely@boystown.org>
2018-04-24 08:35:38 -0500

    Add files via upload
    Check for processing error.

--- afc_demo/afc_demo.m
+++ afc_demo/afc_demo.m
@@ -1,3 +1,5 @@
-iirfb                                  % generate IIR filter coefficients
-system('tst_iffb test/carrots.wav');   % Apply AFC processing
-tst_iffb                               % Show results
+iirfb                                  % Generate IIR filter coefficients
+e=system('tst_iffb test/carrots.wav'); % Apply AFC processing
+if (e==0)                              % Check for error
+    tst_iffb                           % Show results
+end
