4089036e8f96fdf57ca827911c36c690fc3b51af
Stephen Neely <Stephen.Neely@boystown.org>
2018-05-12 15:29:43 -0500

    Add files via upload
    Eliminated dependence on MATLAB for design of IIR filterbank

--- afc_demo/afc_demo.m
+++ afc_demo/afc_demo.m
@@ -1,4 +1,3 @@
-iirfb                                  % Generate IIR filter coefficients
 e=system('tst_iffb test/carrots.wav'); % Apply AFC processing
 if (e==0)                              % Check for error
     tst_iffb                           % Show results
